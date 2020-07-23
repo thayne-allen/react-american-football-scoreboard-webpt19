00f1107f61aca473f2dd60e1af945dad761cc12c
thayne-allen <thayne.allen@gmail.com>
2020-07-23 14:59:02 -0600

    added code for timer

--- src/App.js
+++ src/App.js
@@ -25,13 +25,15 @@ function App() {
           <div className="home">
             <h2 className="home__name">Lions</h2>
 
+
+
             {/* TODO STEP 3 - We need to change the hardcoded values in these divs to accept dynamic values from our state. */}
-            <div className="home__score">32</div>
+            <div className="home__score">{homeCount}</div>
           </div>
-          <div className="timer">00:03</div>
+          <div className="timer"><span id="time">00:03</span></div>
           <div className="away">
             <h2 className="away__name">Tigers</h2>
-            <div className="away__score">32</div>
+            <div className="away__score">{awayCount}</div>
           </div>
         </div>
         <BottomRow />
@@ -40,12 +42,12 @@ function App() {
         <div className="homeButtons">
 
           {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
-          <button className="homeButtons__touchdown">Home Touchdown</button>
-          <button className="homeButtons__fieldGoal">Home Field Goal</button>
+          <button className="homeButtons__touchdown" onClick={() => setHomeCount(homeCount + 7)}>Home Touchdown</button>
+          <button className="homeButtons__fieldGoal" onClick={() => setHomeCount(homeCount + 3)}>Home Field Goal</button>
         </div>
         <div className="awayButtons">
-          <button className="awayButtons__touchdown">Away Touchdown</button>
-          <button className="awayButtons__fieldGoal">Away Field Goal</button>
+          <button className="awayButtons__touchdown" onClick={() => setAwayCount(awayCount + 7)}>Away Touchdown</button>
+          <button className="awayButtons__fieldGoal" onClick={() => setAwayCount(awayCount + 3)}>Away Field Goal</button>
         </div>
       </section>
     </div>
