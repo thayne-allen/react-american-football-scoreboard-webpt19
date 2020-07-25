7a8b3dca362f8605ee0dfe337608a6d2a83793b2
thayne-allen <thayne.allen@gmail.com>
2020-07-24 19:47:27 -0600

    finishing touches

--- src/App.js
+++ src/App.js
@@ -7,14 +7,10 @@ function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
 
   // useState hooks for home.
-  const usingHomeState = useState(0);
-  const setHomeStateVar = usingHomeState[1];
   
   const [homeCount, setHomeCount] = useState(24);
 
   // useState hooks for away
-  const usingAwayState = useState(0);
-  const setAwayStateVar = usingAwayState[1];
 
   const [awayCount, setAwayCount] = useState(17);
 
@@ -25,12 +21,9 @@ function App() {
           <div className="home">
             <h2 className="home__name">Lions</h2>
 
-
-
-            {/* TODO STEP 3 - We need to change the hardcoded values in these divs to accept dynamic values from our state. */}
             <div className="home__score">{homeCount}</div>
           </div>
-          <div className="timer"><span id="time">00:03</span></div>
+          <div className="timer"><span id="time">15:00</span></div>
           <div className="away">
             <h2 className="away__name">Tigers</h2>
             <div className="away__score">{awayCount}</div>
@@ -40,8 +33,6 @@ function App() {
       </section>
       <section className="buttons">
         <div className="homeButtons">
-
-          {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
           <button className="homeButtons__touchdown" onClick={() => setHomeCount(homeCount + 7)}>Home Touchdown</button>
           <button className="homeButtons__fieldGoal" onClick={() => setHomeCount(homeCount + 3)}>Home Field Goal</button>
         </div>
